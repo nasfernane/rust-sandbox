4ef15923a3dbc1bb0b72722928557f5238d6fc60
nasfernane <nasfernane@gmail.com>
2026-09-06 11:59:44 +0200

    feat: variables and mutability

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 fn main() {
-    // line comment
-    /*
-     * like comments
-     * omg omg omg
-     */
-    println!("Hello, World !");
-    let x = 5 + /* block comment within expression */ 5;
-    println!("Is x 10 or 100 ? x = {}", x);
+    // en rust les variables sont immutables par défaut. il faut ajouter mut pour les rendre mutable
+    // let mut x = 5;
+    // println!("X = {}", x);
+
+    // x = 6;
+    // println!("The value of x is {x}");
+
+    // les constantes sont toujours immuables et doivent toujours avoir un type
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // on peut shadow une variable let immutable en la redéclarant. ça permet de changer son type qui est inféré
+    // let spaces = "didadidadoum";
+
+    // {
+    //     let spaces = spaces.len();
+    //     println!("spaces length in inner scope is {spaces}");
+    // }
+
+    // // le shadow disparait en dehors de son scope, la variable revient à sa valeur initiale
+    // println!("spaces length is {spaces}");
+    // 
+
+    // par contre on ne peut pas changer le type d'un let mutable
+
+    let mut spaces = "coucou";
+    spaces = spaces.len(); // error[E0308]: mismatched types
 }
