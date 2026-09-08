2af1f442f6948f5044ed34011877f21fdefd25aa
nasfernane <nasfernane@gmail.com>
2026-09-08 18:43:16 +0200

    feat: functions

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
-use std::io;
-
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    println!("Hello, world!");
 
-    println!("Please enter an array index.");
+    print_number(5);
+    print_labelled_number(45, 'h');
 
-    let mut index = String::new();
+    // FONCTIONS
 
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
+    // différence entre déclaration et expression
+    // une déclaration ne retourne pas de valeur, on ne peut donc pas faire
+    // let x = let y = 6; => error: expected expression, found `let` statement
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
+    // une expression peut faire partie d'une déclaration mais retourne une valeur
+    // 5 + 6 est une expression qui s'évalue à 11
+    // appeler une fonction, une macro ou l'ouverture d'un bloc sont des expressions
+    //
+    // let y = {
+    //     let x = 3;
+    //     x + 1 // pas besoin d'accolade ici, c'est la valeur retournée par le bloc
+    // };
 
-    let element = a[index];
+    // println!("The value of y is {y}");
 
-    println!("The value of the element at index {index} is: {element}");
+    // fonctions avec valeurs de retours
+    // function_name(...parameters) -> return_type {}
+    // fn amazing_function() -> i8 {
+    //     6
+    // }
+
+    // let amazing_number = amazing_function();
+
+    // println!("My amazing number is {amazing_number}");
+
+    let incremental_number = add_one(5);
+    println!("Incremental number is {incremental_number}");
+}
+
+fn print_number(number: i32) {
+    println!("Wow {number} is a beautiful number");
+}
+
+fn print_labelled_number(value: i32, unit_label: char) {
+    println!("Wow {value}{unit_label} is a a lot of {unit_label}");
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1 // si on ajoute un semi-colon => ^^^ expected `i32`, found `()` car la fonction ne retourne plus rien
+    // return ne doit être utilisé que pour des retours anticipés
 }
