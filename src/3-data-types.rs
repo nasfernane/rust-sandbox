82c424f1367ba5cd1430c5994030c7d833392cec
nasfernane <nasfernane@gmail.com>
2026-09-08 17:47:08 +0200

    feat: accessing index out of memory

---
+++ src/3-data-types.rs
@@ -0,0 +1,63 @@
+fn main() {
+    // pour les variables locales (let), l'annotation de type est requise (et souhaitable) seulement quand le type ne peut être correctement inféré exemple avec parse
+    // let guess = "42".parse().expect("Not a number"); // error[E0284]: type annotations needed
+
+    // quand le type est ambigu (ne peut pas être inféré de manière fiable), l'annotation de type est nécessaire
+    let _guess: u32 = "42".parse().expect("Not a number"); // le préfixe _ signifie que la non utilisation de la variable est intentionnelle, pas de warning remonté par le compilateur
+
+    // SCALAIRES
+    // un type scalaire représente une seule valeur. Les types scalaires en rust sont: integers, floating-point numbers, Booleans, et characters
+
+    // Integers
+    // i = signed, u => unsigned;
+    // default integer value is u32
+    // let _value1: i8 // 8bit signed de -128 à +127
+    // let _value2: u8 // 8 bit unsigned de 0 à 255
+
+    // isize et usize dépendent de l'architecture de l'ordinateur qui fait tourner le programme. 64bits sur une architecture 64-bit ou 32 bit sur une archi 32-bit
+
+    // Floating numbers; la valeur par défaut est f64 car plus ou moins aussi rapide mais avec davantage de précision
+    // let x = 2.0; // f64
+    // let y: f32 = 3.0; // f32
+
+    // boolean
+    // let t = true;
+    // let f: bool = false; // annotation explicite
+
+    // character - valeur scalaire unicode; il se déclare avec des guillemets simples
+    // let c = 'z';
+    // let z: char = 'Z'; // annotation explicite
+    // let heart_eyed_cat = '😻';
+    // let oupss = "z" // type string pas char, à cause des guillements doubles
+
+    // types composés (COMPOUND TYPES)
+
+    // les tuples
+    // let tup: (i32, f64, u8) = (500, 6.4, 1); // plusieurs types possibles
+    // on peut déstructurer les valzurs
+    // let (x, y, z) = tup;
+    // println!("The value of y is {y}");
+    // println!("The value of z is {}", z);
+
+    // accéder à l'index d'un tuple
+    // let five_hundred = tup.0;
+    // println!("{five_hundred}");
+
+    // les tableaux - comme tuple mais toutes les valeurs ont le même type
+    // ils sont utiles quand la longueur est fixe (sinon utiliser les vecteurs);
+    // let a = [1, 2, 3, 4];
+
+    // let two = a[1];
+    // println!("{two}");
+
+    // annotation de type = [type; longueur]
+    // let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // répétition de valeurs => [valeur; longueur]
+    let array = [3; 5]; // b vaut [3, 3, 3, 3, 3];
+    println!("third value of array: {}", array[3]);
+
+    // accès comme en js
+    let _first_value = array[0];
+    let _second_value = array[1];
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,22 @@
+use std::io;
+
 fn main() {
-  // pour les variables locales (let), l'annotation de type est requise (et souhaitable) seulement quand le type ne peut être correctement inféré exemple avec parse
-  // let guess = "42".parse().expect("Not a number"); // error[E0284]: type annotations needed
+    let a = [1, 2, 3, 4, 5];
 
-  // quand le type est ambigu (ne peut pas être inféré de manière fiable), l'annotation de type est nécessaire
-  let _guess: u32 = "42".parse().expect("Not a number"); // le préfixe _ signifie que la non utilisation de la variable est intentionnelle, pas de warning remonté par le compilateur
+    println!("Please enter an array index.");
 
-  // SCALAIRES
-  // un type scalaire représente une seule valeur. Les types scalaires en rust sont: integers, floating-point numbers, Booleans, et characters
+    let mut index = String::new();
 
-  // Integers
-  // i = signed, u => unsigned;
-  // default integer value is u32
-  // let _value1: i8 // 8bit signed de -128 à +127
-  // let _value2: u8 // 8 bit unsigned de 0 à 255
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
 
-  // isize et usize dépendent de l'architecture de l'ordinateur qui fait tourner le programme. 64bits sur une architecture 64-bit ou 32 bit sur une archi 32-bit
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
 
-  // Floating numbers; la valeur par défaut est f64 car plus ou moins aussi rapide mais avec davantage de précision
-  // let x = 2.0; // f64
-  // let y: f32 = 3.0; // f32
+    let element = a[index];
 
-  // boolean
-  // let t = true;
-  // let f: bool = false; // annotation explicite
-
-
-  // character - valeur scalaire unicode; il se déclare avec des guillemets simples
-  let c = 'z';
-  let z: char = 'Z'; // annotation explicite
-  let heart_eyed_cat = '😻';
-  let oupss = "z" // type string pas char, à cause des guillements doubles
-
-
+    println!("The value of the element at index {index} is: {element}");
 }
